dbcf780004349dc343a7c181f84853f0b2bcaee0
Rafael Garcia <rafael@enzyme.com>
2022-11-25 17:44:33 -0300

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import "@ionic/react/css/display.css"
 
 /* Theme variables */
 import "./theme/variables.css"
+import "./theme/tailwind.css"
 
 /* App specific */
 import { Amplify } from "aws-amplify"
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx,css}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
